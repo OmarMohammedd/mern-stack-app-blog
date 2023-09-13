a1b9cc54c707c898fddb5ccbadb10a61f0c66355
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2023-09-13 23:05:00 +0300

    update

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "https://breakable-elk-overalls.cyclic.app/"
+    baseURL: "https://kind-gray-shark-veil.cyclic.app/"
 });
 
 export default request;
