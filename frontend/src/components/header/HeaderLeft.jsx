43efcf4f0956c333b6d2c02e9e05ccc40420a99a
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2023-09-14 21:38:17 +0300

    Update HeaderLeft.jsx

--- frontend/src/components/header/HeaderLeft.jsx
+++ frontend/src/components/header/HeaderLeft.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 const HeaderLeft = ({ toggle, setToggle }) => {
   return (
     <div className="header-left">
       <div className="header-logo">
-        <strong>BLOG</strong>
+         <Link to="/" style={{color: "#fff"}}><strong>BLOG</strong></Link>
         <i className="bi bi-pencil"></i>
       </div>
       <div onClick={() => setToggle((prev) => !prev)} className="header-menu">
