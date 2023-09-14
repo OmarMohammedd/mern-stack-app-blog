fb869444e7f4a49af539720b3ebd0b2f4e1cf8be
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2023-09-14 21:29:53 +0300

    Update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "https://cute-lime-chinchilla-shoe.cyclic.cloud"
+    baseURL: "https://cute-lime-chinchilla-shoe.cyclic.cloud/"
    // baseURL: "http://localhost:8000"
 });
 
